4060194196873b669f4719b7d89ad5b70946d9c9
Guilherme Martins <g.santosmartins@outlook.com>
2022-11-16 13:14:36 -0300

    refactor: explict http request response status

--- src/main/java/org/acme/sequence/SequenceResource.java
+++ src/main/java/org/acme/sequence/SequenceResource.java
@@ -6,10 +6,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
+import org.jboss.resteasy.reactive.ResponseStatus;
+
 @Path("v1")
 public class SequenceResource {
 
     @GET
+    @ResponseStatus(200)
     @Path("/alticci/{param}")
     public HashMap<String, Long> squenceService(@PathParam("param") int param) {
         // new CacheService().load(param, result);
